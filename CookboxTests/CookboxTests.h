7dac10abbed410c411045e9c5cfd6160050937d2
Ethan James <ejames@Administrators-Mac-mini-3.local>
2012-10-26 14:29:40 -0400

    Initial Commit

---
+++ CookboxTests/CookboxTests.h
@@ -0,0 +1,13 @@
+//
+//  CookboxTests.h
+//  CookboxTests
+//
+//  Created by  on 10/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CookboxTests : SenTestCase
+
+@end
---
+++ CookboxTests/CookboxTests.m
@@ -0,0 +1,32 @@
+//
+//  CookboxTests.m
+//  CookboxTests
+//
+//  Created by  on 10/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CookboxTests.h"
+
+@implementation CookboxTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CookboxTests");
+}
+
+@end
